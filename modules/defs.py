adea33e1efcff22822017b33926f0003b03e0d36
David <david-bitner@hotmail.com>
2020-10-27 17:33:56 -0300

    Create defs.py
    Criado o arquivo que vai comportar todo o código bruto já com a função deletar adicionada a ele

---
+++ modules/defs.py
@@ -0,0 +1,128 @@
+def separador():
+    print('*' * 50)
+
+
+def print_centralizado(txt):
+    print(f'{txt}'.center(50))
+
+
+def print_personalizado(txt):
+    print('*' * 50)
+    print_centralizado(txt)
+    print('*' * 50)
+
+
+def continuar():
+    input('Pressione enter para continuar...')
+
+
+def gerar_numero(txt='lul'):
+    entrada = int(input(txt))
+    return entrada
+
+
+def adicionar_texto():
+    separador()
+    append = str(input('Escreva o que deseja adicionar ao arquivo:\n'))
+    with open('main.txt', 'a') as arquivo:
+        arquivo.write(f'{append}\n')
+    print('Texto adicionado com sucesso')
+
+
+def ver_conteudo():
+    separador()
+    with open('main.txt', 'r') as arquivo:
+        conteudo = arquivo.read()
+    print(conteudo)
+    continuar()
+
+
+def ver_conteudo_linha():
+    while True:
+        separador()
+        opcoes_vcl = ['1 - Selecionar linha especifica', '2 - Selecionar um intervalo de linhas', '69 - Voltar']
+        for o_vcl in opcoes_vcl:
+            print(o_vcl)
+        opcao_vcl = gerar_numero('Entrada: ')
+        if opcao_vcl == 1:
+            separador()
+            with open('main.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
+            print(f'O arquivo possui {len(linhas) + 1} linhas')
+            linha_selecionada = gerar_numero('Linha selecionada: ')
+            try:
+                print(linhas[linha_selecionada - 1])
+            except IndexError:
+                print_personalizado('Numero acima do limite de linhas do arquivo')
+            continuar()
+        elif opcao_vcl == 2:
+            separador()
+            with open('main.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
+            print(f'O arquivo possui {len(linhas) + 1} linhas')
+            primeira_linha = gerar_numero('Primeira linha: ')
+            ultima_linha = gerar_numero('Ultima linha: ')
+            for i in range(1, len(linhas) + 1):
+                if primeira_linha <= i <= ultima_linha:
+                    print(f'Linha {i}: {linhas[i - 1]}', end='')
+            continuar()
+        elif opcao_vcl == 69:
+            break
+
+
+def procurar():
+    pass
+
+
+def deletar():
+    while True:
+        separador()
+        opcoes_del = ['1 - Deletar linha', '2 - Deletar sentença', '3 - Deletar tudo',
+                      '69 - Voltar']
+        for o_del in opcoes_del:
+            print(o_del)
+        opcao_del = gerar_numero('Entrada: ')
+        if opcao_del == 1:
+            separador()
+            with open('main.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
+            for index, linha in enumerate(linhas):
+                print(f'{index} - {linha}', end='')
+            linha_deletada = gerar_numero('Linha que será apagada: ')
+            linhas.pop(linha_deletada)
+            with open('main.txt', 'w') as arquivo:
+                for linha in linhas:
+                    arquivo.write(linha)
+            continuar()
+        elif opcao_del == 2:
+            separador()
+            apagar = False
+            apagar_quantidade = 0
+            with open('main.txt', 'r') as arquivo:
+                linhas = arquivo.readlines()
+            sentenca_deletada = str(input('Digite o que deseja apagar: '))
+            for index, linha in enumerate(linhas):
+                if sentenca_deletada in linha:
+                    nova_linha = linha.replace(f'{sentenca_deletada}', '')
+                    linhas[index] = nova_linha
+                    apagar = True
+                    apagar_quantidade += 1
+                    if linhas[index] == '\n':
+                        linhas.pop(index)
+            if not apagar:
+                print('Esta sentença não existe dentro do arquivo de texto!')
+            elif apagar:
+                with open('main.txt', 'w') as arquivo:
+                    for linha in linhas:
+                        arquivo.write(linha)
+                print(f'Sentença: {sentenca_deletada} apagada com sucesso {apagar_quantidade} ', end='')
+                print('vezes') if apagar_quantidade > 1 else print('vez')
+            continuar()
+        elif opcao_del == 3:
+            separador()
+            with open('main.txt', 'w') as arquivo:
+                arquivo.write('')
+            print('Arquivo Limpado com sucesso')
+            continuar()
+        elif opcao_del == 69:
+            break
